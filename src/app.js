9db2832d310467a6cc72ec537160e21f8fc175e3
vigzmv <vigzmv@outlook.com>
2017-11-08 02:25:44 +0530

    +IPFS-CRDT

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
-console.log(1);
+const IPFS = require('ipfs');
+const Yjs = require('yjs');
+require('y-memory')(Yjs);
+require('y-array')(Yjs);
+require('y-text')(Yjs);
+require('y-ipfs-connector')(Yjs);
+
+function repo() {
+  return `ipfs/Yjs-demo/${Math.random()}`;
+}
+
+const ipfs = new IPFS({
+  repo: repo(),
+  EXPERIMENTAL: {
+    pubsub: true,
+  },
+});
+
+ipfs.once('ready', () =>
+  ipfs.id((err, info) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`IPFS node ready with address ${info.id}`);
+
+    Yjs({
+      db: {
+        name: 'memory',
+      },
+      connector: {
+        name: 'ipfs',
+        room: 'ipfs-crdt',
+        ipfs,
+      },
+      share: {
+        textfield: 'Text',
+      },
+    }).then((y) => {
+      y.share.textfield.bind(document.getElementById('textfield'));
+    });
+  }));
