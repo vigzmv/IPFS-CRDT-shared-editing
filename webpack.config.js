af16f5e06c6fe3e3e91374d44aba772f1fdd7860
vigzmv <vigzmv@outlook.com>
2017-11-08 01:58:23 +0530

    +Webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'docs/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['es2015', { modules: false }]],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
